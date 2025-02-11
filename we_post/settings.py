b1161852365a406c4c33cafe58f732752de700a3
Yahya Mohamed <yahya.hm750@gmail.com>
2025-02-11 16:33:54 +0000

    add secret key and prepare for production

--- we_post/settings.py
+++ we_post/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%cdd*mu-y^*#k&dgm+=f$=ei%kl_2tsk%l#a2fj+vn4&t&%uzt'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
