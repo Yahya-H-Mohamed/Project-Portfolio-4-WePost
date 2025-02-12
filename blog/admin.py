e8037175055dbbf36cc57eea1777e05df66ae43e
Yahya Mohamed <yahya.hm750@gmail.com>
2025-02-12 15:39:22 +0000

    add summernote and loaded data to database

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    list_display = ('post_title', 'author', 'created_on')
+    list_filter = ('created_on',)
+    search_fields = ['post_title', 'content']  
+    summernote_fields = ('content',)
+
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)
