9c8e2b03b8443db5fa6f9b9f76e3785933678bb5
Yahya Mohamed <yahya.hm750@gmail.com>
2025-06-20 04:47:46 +0100

    test postlist view for get request

--- blog/test_views.py
+++ blog/test_views.py
@@ -1,26 +1,37 @@
 from django.contrib.auth.models import User
 from django.urls import reverse
-from django.test import TestCase
-from .form import CommentForm
+from django.test import TestCase, Client
 from .models import Post
 
 class TestBlogViews(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create_superuser(
-            username="myUsername",
-            password="myPassword",
-            email="test@test.com"
+        """
+        Set up users, a post, and a logged-in client for view testing.
+        """
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
         )
-        self.post = Post(post_title="Blog title", author=self.user,
-                         content="Blog content", edited="False")
-        self.post.save()
+        self.other_user = User.objects.create_user(
+            username='otheruser',
+            password='otherpassword'
+        )
+        self.post = Post.objects.create(
+            post_title='A Test Post',
+            author=self.user,
+            content='Some content for testing.'
+        )
+        # Log in client for set up
+        self.client.login(username='testuser', password='testpassword')
 
-    def test_render_post_detail_page_with_comment_form(self):
-        response = self.client.get(reverse(
-            'post_detail', kwargs={'post_id': self.post.id}))
+    def test_post_list_view_get(self):
+        """
+        Tests the PostList view for a GET request.
+        """
+        # 'reverse' gets the URL from its name in urls.py
+        response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b"Blog title", response.content)
-        self.assertIn(b"Blog content", response.content)
-        self.assertIsInstance(
-            response.context['comment_form'], CommentForm)
+        self.assertTemplateUsed(response, 'blog/index.html')
+        self.assertContains(response, 'A Test Post') # Check if post title is in the HTML
