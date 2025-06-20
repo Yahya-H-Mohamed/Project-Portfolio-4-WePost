4908aa4ea4624aaf54b76dcbfe179cd98634f4b6
Yahya Mohamed <yahya.hm750@gmail.com>
2025-06-20 03:11:50 +0100

    create test to check comment form status

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -26,3 +26,19 @@ class TestForms(TestCase):
         self.assertFalse(form.is_valid())
         self.assertIn('post_title', form.errors.keys())
         self.assertEqual(form.errors['post_title'][0], 'This field is required.')
+    
+    def test_comment_form_is_valid(self):
+        """
+        Tests that the CommentForm is valid with correct data.
+        """
+        form = CommentForm({'content': 'This is a valid comment.'})
+        self.assertTrue(form.is_valid())
+
+    def test_comment_form_is_invalid_if_content_missing(self):
+        """
+        Tests that the CommentForm is invalid if the content is empty.
+        """
+        form = CommentForm({'content': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('content', form.errors.keys())
+        self.assertEqual(form.errors['content'][0], 'This field is required.')
