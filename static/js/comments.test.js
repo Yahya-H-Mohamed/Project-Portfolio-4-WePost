5a30d6880eba8f3c5f8d0bb11a8ba40b050be4bb
Yahya Mohamed <yahya.hm750@gmail.com>
2025-06-20 07:38:22 +0100

    add tests for buttons

--- static/js/comments.test.js
+++ static/js/comments.test.js
@@ -23,3 +23,27 @@ beforeEach(() => {
 
   require('./comments.js'); 
 });
+
+test('Clicking edit button populates form and changes submit button', () => {
+  // Simulate clicking the edit button
+  document.querySelector('.btn-edit').click();
+
+  // Expect the textarea is populated
+  expect(document.getElementById('id_content').value).toBe('Test comment 1');
+  // Expect the submit button text is changed
+  expect(document.getElementById('submitButton').innerText).toBe('Update');
+  // Expect the form action is set correctly
+  expect(document.getElementById('commentForm').getAttribute('action')).toBe('edit_comment/1');
+});
+
+test('Clicking edit button populates form and changes submit button', () => {
+  // Simulate clicking the edit button
+  document.querySelector('.btn-edit').click();
+
+  // Expect the textarea is populated
+  expect(document.getElementById('id_content').value).toBe('Test comment 1');
+  // Expect the submit button text is changed
+  expect(document.getElementById('submitButton').innerText).toBe('Update');
+  // Expect the form action is set correctly
+  expect(document.getElementById('commentForm').getAttribute('action')).toBe('edit_comment/1');
+});
