36d256107ad4e7c59ee06c39e6e31773ee08168c
Yahya Mohamed <yahya.hm750@gmail.com>
2025-06-20 02:51:23 +0100

    set up dummy user for tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Post, Comment
 
-# Create your tests here.
+class TestModels(TestCase):
+
+    def setUp(self):
+        """
+        Set up a user
+        This runs before every test function.
+        """
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.post = Post.objects.create(
+            post_title='A Test Post',
+            author=self.user,
+            content='Some content for testing.',
+            category='technology'
+        )
