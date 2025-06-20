47a1b8dc36a895192213c4b530fa896be10b2126
Yahya Mohamed <yahya.hm750@gmail.com>
2025-06-20 03:10:30 +0100

    create test to check post form status

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,14 +1,28 @@
 from django.test import TestCase
-from .forms import CommentForm
+from .forms import PostForm, CommentForm
 
+class TestForms(TestCase):
 
-class TestCommentForm(TestCase):
+    def test_post_form_is_valid(self):
+        """
+        Tests that the PostForm is valid with correct data
+        """
+        form = PostForm({
+            'post_title': 'A valid title',
+            'category': 'education',
+            'content': 'Some valid content.'
+        })
+        self.assertTrue(form.is_valid())
 
-    def test_form_is_valid(self):
-        comment_form = CommentForm({'content': 'This is a great post'})
-        self.assertTrue(comment_form.is_valid())
-        self.assertTrue(comment_form.is_valid(), msg="Form is invalid")
-
-    def test_form_is_invalid(self):
-        comment_form = CommentForm({'content': ''})
-        self.assertFalse(comment_form.is_valid(), msg="Form is valid")
+    def test_post_form_is_invalid_if_title_missing(self):
+        """
+        Tests that the PostForm is invalid if the title is empty
+        """
+        form = PostForm({
+            'post_title': '', 
+            'category': 'other',
+            'content': 'Content without a title.'
+        })
+        self.assertFalse(form.is_valid())
+        self.assertIn('post_title', form.errors.keys())
+        self.assertEqual(form.errors['post_title'][0], 'This field is required.')
